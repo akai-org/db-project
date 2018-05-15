b87f49f32b95fb219c65c918c5c2b1b3336139bf
mdziardziel <mdziardziel@gmail.com>
2018-05-15 22:24:02 +0200

    members api

---
+++ priv/repo/seeds_members.exs
@@ -0,0 +1,7 @@
+DbProject.Repo.insert!(%DbProject.Members.Member{name: "Jim", surname: "Joe", github: "www.google.com"})
+DbProject.Repo.insert!(%DbProject.Members.Member{name: "Jimm", surname: "Joel", github: "www.google.com"})
+DbProject.Repo.insert!(%DbProject.Members.Member{name: "Jimmi", surname: "Joeli", github: "www.google.com"})
+
+DbProject.Repo.insert!(%DbProject.FormerMembers.FormerMember{name: "Gin", surname: "Bro", github: "www.google.com"})
+DbProject.Repo.insert!(%DbProject.FormerMembers.FormerMember{name: "Gini", surname: "Broe", github: "www.google.com"})
+DbProject.Repo.insert!(%DbProject.FormerMembers.FormerMember{name: "Ginni", surname: "Broew", github: "www.google.com"})
